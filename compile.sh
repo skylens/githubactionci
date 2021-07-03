db28ebb2df2b375cb2c1eb995c6481e2ae2d52e1
skylens <skylens116@outlook.com>
2021-07-03 20:45:27 +0800

    update

--- compile.sh
+++ compile.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-brew install automake gsed upx
+brew install automake gsed
+brew install go@1.16.5
+brew link --overwrite go
 
-# wget https://sourceforge.net/projects/ijbswa/files/Sources/3.0.32%20%28stable%29/privoxy-3.0.32-stable-src.tar.gz
-# tar zxf privoxy-3.0.32-stable-src.tar.gz
-# cd privoxy-3.0.32-stable-src
+wget https://sourceforge.net/projects/ijbswa/files/Sources/3.0.32%20%28stable%29/privoxy-3.0.32-stable-src.tar.gz
+tar zxf privoxy-3.0.32-stable-src.tar.gz
+cd privoxy-3.0.32-stable-src
 # git clone https://www.privoxy.org/git/privoxy.git
 # cd privoxy
-# autoheader
-# autoconf
-# ./configure --prefix=/Users/runner/project/dists/privoxy 
-# make
+autoheader
+autoconf -i
+./configure --prefix=/Users/runner/project/dists/privoxy 
+make
 
 ver=2.16.6
 wget --no-check-certificate https://tls.mbed.org/download/mbedtls-$ver-gpl.tgz
 tar zxf mbedtls-$ver-gpl.tgz
 cd mbedtls-$ver
-# cmake -DUSE_SHARED_MBEDTLS_LIBRARY=On -DPython3_EXECUTABLE=/usr/bin/python3
 gsed -i "s/DESTDIR=\/usr\/local/DESTDIR=\/Users\/runner\/project\/dists\/mbedtls/g" Makefile
-# make SHARED=0 && make install
 make lib && make install
 cp /Users/runner/project/dists/mbedtls/lib/libmbedcrypto.dylib /Users/runner/project/dists/shadowsocks-libev/bin/
 cd ..
@@ -72,7 +72,9 @@ git clone https://github.com/shadowsocks/simple-obfs
 cd simple-obfs
 git submodule init && git submodule update
 ./autogen.sh
-./configure --prefix=/Users/runner/project/dists/shadowsocks-libev --disable-documentation
+./configure --prefix=/Users/runner/project/dists/shadowsocks-libev \
+--disable-documentation \
+--with-ev=/Users/runner/project/dists/libev
 make && make install
 cd ..
 
@@ -89,13 +91,6 @@ env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 \
 go build -v -ldflags "-X main.VERSION=$(git describe --tags) -s -w -buildid=" -o ck-client-darwin ./cmd/ck-client
 cp ck-client-darwin /Users/runner/project/dists/shadowsocks-libev/bin/ck-client
 cd ..
-
-# upx
-cd /Users/runner/project/dists/shadowsocks-libev/bin/
-upx ss-local
-upx obfs-local
-upx v2ray-plugin
-upx ck-client
 
 cd /Users/runner/project/
 

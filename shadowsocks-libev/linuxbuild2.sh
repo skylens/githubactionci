84abe41628bf2e94a4a5401c9007ef8190bf0c17
skylens <skylens116@outlook.com>
2021-08-17 23:24:32 +0800

    update

--- shadowsocks-libev/linuxbuild2.sh
+++ shadowsocks-libev/linuxbuild2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ver=1.2.11
-# wget https://zlib.net/zlib-$ver.tar.gz
+wget https://zlib.net/zlib-$ver.tar.gz
 tar -xvf zlib-$ver.tar.gz
 cd zlib-$ver
 ./configure --prefix=/home/runner/project/dists/zlib --static --libdir=/home/runner/project/dists/zlib/lib
@@ -9,7 +9,7 @@ make && make install
 cd ..
 
 ver=8.45
-# wget https://ftp.pcre.org/pub/pcre/pcre-$ver.tar.gz
+wget https://ftp.pcre.org/pub/pcre/pcre-$ver.tar.gz
 tar zxf pcre-$ver.tar.gz
 cd pcre-$ver
 ./configure --prefix=/home/runner/project/dists/pcre --disable-shared --enable-utf8 --enable-unicode-properties
@@ -17,15 +17,15 @@ make && make install
 cd ..
 
 ver=2.16.6
-# wget --no-check-certificate https://tls.mbed.org/download/mbedtls-$ver-gpl.tgz
+wget --no-check-certificate https://tls.mbed.org/download/mbedtls-$ver-gpl.tgz
 tar zxf mbedtls-$ver-gpl.tgz
 cd mbedtls-$ver
-LDFLAGS=-static make DESTDIR=/home/runner/project/dists/mbedtls install
-# make lib && make install
+sed -i "s/DESTDIR=\/usr\/local/DESTDIR=\/home\/runner\/project\/dists\/mbedtls/g" Makefile
+make lib && make install
 cd ..
 
 ver=1.0.18
-# wget --no-check-certificate https://download.libsodium.org/libsodium/releases/libsodium-$ver.tar.gz
+wget --no-check-certificate https://download.libsodium.org/libsodium/releases/libsodium-$ver.tar.gz
 tar zxf libsodium-$ver.tar.gz
 cd libsodium-$ver
 ./configure --prefix=/home/runner/project/dists/libsodium --disable-shared
@@ -33,7 +33,7 @@ make && make install
 cd ..
 
 ver=4.33
-# wget http://dist.schmorp.de/libev/libev-$ver.tar.gz 
+wget http://dist.schmorp.de/libev/libev-$ver.tar.gz 
 tar zxf libev-$ver.tar.gz
 cd libev-$ver
 ./configure --prefix=/home/runner/project/dists/libev --disable-shared
@@ -41,7 +41,7 @@ make && make install
 cd ..
 
 ver=1.17.1
-# wget https://c-ares.haxx.se/download/c-ares-$ver.tar.gz
+wget https://c-ares.haxx.se/download/c-ares-$ver.tar.gz
 tar zxf c-ares-$ver.tar.gz
 cd c-ares-$ver
 ./configure --prefix=/home/runner/project/dists/cares --disable-shared
@@ -52,8 +52,7 @@ git clone https://github.com/shadowsocks/shadowsocks-libev
 cd shadowsocks-libev
 git submodule init && git submodule update
 ./autogen.sh
-# LDFLAGS="-static" CFLAGS="-static" \
-LIBS="-lpthread -lm" LDFLAGS="-Wl,-static -static -static-libgcc -L/home/runner/project/dists/libsodium/lib" CFLAGS="-I/home/runner/project/dists/libsodium/include" \
+LDFLAGS="-static" CFLAGS="-static" \
 ./configure --prefix=/home/runner/project/dists/shadowsocks-libev \
 --disable-documentation \
 --with-mbedtls=/home/runner/project/dists/mbedtls \
@@ -94,3 +93,6 @@ cp /home/runner/project/dists/shadowsocks-libev/bin/ss-* build/releases
 cp /home/runner/project/dists/shadowsocks-libev/bin/obfs-local build/releases
 cp /home/runner/project/dists/shadowsocks-libev/bin/v2ray-plugin build/releases
 cp /home/runner/project/dists/shadowsocks-libev/bin/ck-client build/releases
+
+cd build/releases
+tar -zcvf shadowsocks-libev-ubuntu.tgz *

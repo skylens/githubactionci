7dcdaad93764458b9ec27f4bc5cd67ec1feddc45
skylens <skylens116@outlook.com>
2021-08-18 09:35:07 +0800

    update

--- ss-libev/linuxbuild.sh
+++ ss-libev/linuxbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ver=1.2.11
-# wget https://zlib.net/zlib-$ver.tar.gz
+wget https://zlib.net/zlib-$ver.tar.gz
 tar -xvf zlib-$ver.tar.gz
 cd zlib-$ver
 ./configure --prefix=/home/runner/project/dists/zlib --static --libdir=/home/runner/project/dists/zlib/lib
@@ -9,7 +9,7 @@ make && make install
 cd ..
 
 ver=8.45
-# wget https://ftp.pcre.org/pub/pcre/pcre-$ver.tar.gz
+wget https://ftp.pcre.org/pub/pcre/pcre-$ver.tar.gz
 tar zxf pcre-$ver.tar.gz
 cd pcre-$ver
 ./configure --prefix=/home/runner/project/dists/pcre --disable-shared --enable-utf8 --enable-unicode-properties
@@ -17,7 +17,7 @@ make && make install
 cd ..
 
 ver=2.16.6
-# wget --no-check-certificate https://tls.mbed.org/download/mbedtls-$ver-gpl.tgz
+wget --no-check-certificate https://tls.mbed.org/download/mbedtls-$ver-gpl.tgz
 tar zxf mbedtls-$ver-gpl.tgz
 cd mbedtls-$ver
 LDFLAGS=-static make DESTDIR=/home/runner/project/dists/mbedtls install
@@ -25,7 +25,7 @@ LDFLAGS=-static make DESTDIR=/home/runner/project/dists/mbedtls install
 cd ..
 
 ver=1.0.18
-# wget --no-check-certificate https://download.libsodium.org/libsodium/releases/libsodium-$ver.tar.gz
+wget --no-check-certificate https://download.libsodium.org/libsodium/releases/libsodium-$ver.tar.gz
 tar zxf libsodium-$ver.tar.gz
 cd libsodium-$ver
 ./configure --prefix=/home/runner/project/dists/libsodium --disable-shared
@@ -33,7 +33,7 @@ make && make install
 cd ..
 
 ver=4.33
-# wget http://dist.schmorp.de/libev/libev-$ver.tar.gz 
+wget http://dist.schmorp.de/libev/libev-$ver.tar.gz 
 tar zxf libev-$ver.tar.gz
 cd libev-$ver
 ./configure --prefix=/home/runner/project/dists/libev --disable-shared
@@ -41,7 +41,7 @@ make && make install
 cd ..
 
 ver=1.17.1
-# wget https://c-ares.haxx.se/download/c-ares-$ver.tar.gz
+wget https://c-ares.haxx.se/download/c-ares-$ver.tar.gz
 tar zxf c-ares-$ver.tar.gz
 cd c-ares-$ver
 ./configure --prefix=/home/runner/project/dists/cares --disable-shared
@@ -77,16 +77,16 @@ cd ..
 
 git clone https://github.com/shadowsocks/v2ray-plugin
 cd v2ray-plugin
-env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 \
-go build -v -ldflags "-X main.VERSION=$(git describe --abbrev=0 --tags) -s -w -buildid=" -o v2ray-plugin-darwin
-cp v2ray-plugin-darwin /home/runner/project/dists/shadowsocks-libev/bin/v2ray-plugin
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
+go build -v -ldflags "-X main.VERSION=$(git describe --abbrev=0 --tags) -s -w -buildid=" -o v2ray-plugin-linux
+cp v2ray-plugin-linux /home/runner/project/dists/shadowsocks-libev/bin/v2ray-plugin
 cd ..
 
 git clone https://github.com/cbeuw/Cloak
 cd Cloak
-env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 \
-go build -v -ldflags "-X main.VERSION=$(git describe --abbrev=0 --tags) -s -w -buildid=" -o ck-client-darwin ./cmd/ck-client
-cp ck-client-darwin /home/runner/project/dists/shadowsocks-libev/bin/ck-client
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
+go build -v -ldflags "-X main.VERSION=$(git describe --abbrev=0 --tags) -s -w -buildid=" -o ck-client-linux ./cmd/ck-client
+cp ck-client-linux /home/runner/project/dists/shadowsocks-libev/bin/ck-client
 cd ..
 
 mkdir -p build/releases
@@ -94,3 +94,6 @@ cp /home/runner/project/dists/shadowsocks-libev/bin/ss-* build/releases
 cp /home/runner/project/dists/shadowsocks-libev/bin/obfs-local build/releases
 cp /home/runner/project/dists/shadowsocks-libev/bin/v2ray-plugin build/releases
 cp /home/runner/project/dists/shadowsocks-libev/bin/ck-client build/releases
+
+cd build/releases
+tar -zcvf shadowsocks-libev-ubuntu.tgz *

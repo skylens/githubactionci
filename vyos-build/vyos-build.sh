ea53b7a16267484d9aa06ab07471fd252ebd6814
skylens <skylens116@outlook.com>
2022-10-24 09:24:23 +0800

    vyos build add

---
+++ vyos-build/vyos-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+docker pull vyos/vyos-build:equuleus
+git clone -b equuleus --single-branch https://github.com/vyos/vyos-build
+cd vyos-build
+docker build -t vyos/vyos-build:equuleus docker
+
+# alias vybld_equuleus='docker pull vyos/vyos-build:equuleus && docker run --rm -it \
+#     -v "$(pwd)":/vyos \
+#     -v "$HOME/.gitconfig":/etc/gitconfig \
+#     -v "$HOME/.bash_aliases":/home/vyos_bld/.bash_aliases \
+#     -v "$HOME/.bashrc":/home/vyos_bld/.bashrc \
+#     -w /vyos --privileged --sysctl net.ipv6.conf.lo.disable_ipv6=0 \
+#     -e GOSU_UID=$(id -u) -e GOSU_GID=$(id -g) \
+#     vyos/vyos-build:equuleus bash'
+
+./configure --architecture amd64 --build-by "skylens116@outlook.com"
+make iso
+make vmware
+maek ova

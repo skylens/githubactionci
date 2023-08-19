de25e4d2f575b4003bdb01cc8d9fb09dbb01e2d5
skylens <skylens116@outlook.com>
2023-08-19 19:14:49 +0800

    Update make.sh

--- vyos-build/make.sh
+++ vyos-build/make.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install -y wget libncurses5 tree
 sudo ln -s /lib/x86_64-linux-gnu/libncursesw.so.6 /lib/x86_64-linux-gnu/libncursesw.so.5
 chmod +x VMware-ovftool-4.3.0-15755677-lin.x86_64.bundle
-sudo ./VMware-ovftool-4.3.0-15755677-lin.x86_64.bundle
+sudo ./VMware-ovftool-4.3.0-15755677-lin.x86_64.bundle --console --required --eulas-agreed
 sudo ./configure --architecture amd64 --build-by "skylens116@outlook.com"
 # make iso
 # install ovftools && gen privatekey sign ova

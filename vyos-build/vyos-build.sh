64df91e6c247d528e8447c92f81262fc7e8c3566
skylens <skylens116@outlook.com>
2023-08-20 00:08:05 +0800

    update

--- vyos-build/vyos-build.sh
+++ vyos-build/vyos-build.sh
@@ -3,6 +3,7 @@ set -ex
 docker pull vyos/vyos-build:equuleus
 git clone -b equuleus --single-branch https://github.com/vyos/vyos-build
 cp make.sh vyos-build
+cp template.ovf vyos-build/scripts/
 cd vyos-build
 wget 'https://drive.skylens.cc/api/raw/?path=/software/vmware/VMware-ovftool-4.3.0-15755677-lin.x86_64.bundle' -O VMware-ovftool-4.3.0-15755677-lin.x86_64.bundle
 # docker build -t vyos/vyos-build:equuleus docker

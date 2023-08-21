a18cc5f35866e34d30f2c3a0e3171a03bced1ef0
skylens <skylens116@outlook.com>
2023-08-21 13:50:02 +0800

    Update make.sh

--- vyos-build/make.sh
+++ vyos-build/make.sh
@@ -6,7 +6,10 @@ sudo ./VMware-ovftool-4.3.0-15755677-lin.x86_64.bundle --console --required --eu
 mkdir key
 openssl req -x509 -nodes -sha256 -days 3650 -newkey rsa:1024 -keyout privatekey.pem -out privatekey.pem -subj "/C=US/ST=California/L=Palo Alto/O=VMware/CN=vyos"
 cp privatekey.pem key
-sudo ./configure --architecture amd64 --build-by "skylens116@outlook.com"
-# make iso
+# sudo ./configure --architecture amd64 --build-by "skylens116@outlook.com"
+# https://gitee.com/gas32
+sudo ./configure --architecture amd64 --build-by "skylens116@outlook.com" \
+--version 1.3.3 --build-type release
+make iso
 # install ovftools && gen privatekey sign ova
-sudo make vmware
+make vmware
